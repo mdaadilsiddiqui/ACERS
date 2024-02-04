7584149b6edd7cb91e9643c69179247e660e44f5
mdaadilsiddiqui <mdsiddiquiaadil@gmail.com>
2024-02-04 22:45:03 +0530

    anchor added

---
+++ app.js
@@ -0,0 +1,12 @@
+function openNotes() {
+  window.open("https://www.educative.io/", "_blank");
+}
+function openCourses() {
+  window.open("courses.html", "_blank");
+}
+function openPyq() {
+  window.open("https://www.educative.io/", "_blank");
+}
+function openCareer() {
+  window.open("https://www.educative.io/", "_blank");
+}
